c907df302d9f7682f382dafe25bd2465e46bf047
alonk-me <57488115+alonk-me@users.noreply.github.com>
2020-09-15 18:54:29 +0300

    Update layer1.js

--- Div_layers/layer1.js
+++ Div_layers/layer1.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Demo from './Breakfast_options';
+import Demo from '../Food_options/Breakfast_options';
 
 
-export default function lunchtime() {
+export default function Morning_breakfast() {
   return <div>
   <h1> Breakfast </h1>
   <Demo />
