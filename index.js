aa46d09e7279eeb084fa75e027250fd9d5cfe52a
alonk-me <57488115+alonk-me@users.noreply.github.com>
2020-09-15 18:56:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Layer1 from './layer1'
-import Layer2 from './layer2'
-import Layer3 from './layer3'
+import React from "react";
+import ReactDOM from "react-dom";
+import Layer1 from "./Div_layers/Layer1";
+import Layer2 from "./Div_layers/Layer2";
+import Layer3 from "./Div_layers/Layer3";
 
 function App() {
-  return 
-  <div>
+  return <div>
+  
     <Layer1 />
     <Layer2 />
     <Layer3 />
-    </div>
+  </div>
 };
 
-
-ReactDOM.render(<App />, document.querySelector('#root'));
-    
+ReactDOM.render(<App />, document.querySelector("#root"));
